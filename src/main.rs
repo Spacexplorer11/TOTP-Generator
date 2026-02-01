db6aed9d8a7c5f3dbff56e73a7aa9fdfb0727e67
Akaalroop Singh <akaal@akaalroop.com>
2026-02-01 17:56:32 +0000

    Add the actual working generator of the codes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use std::io;
+use totp_rs::{Algorithm, Secret, TOTP};
+
 fn main() {
     let title_screen_art = r"___  __  ___  __      __   ___       ___  __       ___  __   __
  |  /  \  |  |__)    / _` |__  |\ | |__  |__)  /\   |  /  \ |__)
  |  \__/  |  |       \__> |___ | \| |___ |  \ /~~\  |  \__/ |  \ ";
     println!("{}", title_screen_art);
+
+    println!("Enter your secret (Base32 - Only include A-Z & 2-7):");
+    let mut secret_str = String::new();
+
+    io::stdin()
+        .read_line(&mut secret_str)
+        .expect("An error occurred while taking input");
+
+    let secret_str = secret_str.trim().replace(" ", "").to_uppercase();
+    let secret = Secret::Encoded(secret_str)
+        .to_bytes()
+        .expect("Invalid Base32 secret");
+
+    let totp = TOTP::new(Algorithm::SHA1, 6, 1, 30, secret).unwrap();
+
+    let token = totp.generate_current().unwrap();
+    println!("Current token: {}", token);
 }
