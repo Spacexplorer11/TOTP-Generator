7665b3f360a2517941a9603ebc9ab8b2be11c464
Akaalroop Singh <akaal@akaalroop.com>
2026-02-18 18:54:26 +0000

    Load a .env file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use dotenv::dotenv;
+use std::path::PathBuf;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::{io, thread};
 use totp_rs::{Algorithm, Secret, TOTP};
@@ -7,6 +9,11 @@ fn main() {
  |  /  \  |  |__)    / _` |__  |\ | |__  |__)  /\   |  /  \ |__)
  |  \__/  |  |       \__> |___ | \| |___ |  \ /~~\  |  \__/ |  \ ";
     println!("{}", title_screen_art);
+    if !dotenv().is_ok() {
+        println!("Warning: .env file could not be loaded, saved secrets will not be used.");
+    } else {
+        dotenv().ok();
+    }
 
     println!(
         "Warning: This program clears your terminal after you enter the key, please ensure you have no information you may need later in this terminal session"
