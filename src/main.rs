58fd8c5fe690bcbf3d4dff4c299ef5b4bef25fbc
Akaalroop Singh <akaal@akaalroop.com>
2026-02-19 16:01:27 +0000

    Multiple tokens are now supported!!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use std::path::PathBuf;
+use std::env;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::{io, thread};
 use totp_rs::{Algorithm, Secret, TOTP};
@@ -19,6 +19,7 @@ fn main() {
         "Warning: This program clears your terminal after you enter the key, please ensure you have no information you may need later in this terminal session"
     );
     println!("To abort use CTRL+C");
+    println!("If you wish to use stored / loaded secrets, please enter 'N' ");
     println!("Enter your secret (Base32 - Only include A-Z & 2-7):");
     let mut secret_str = String::new();
 
@@ -27,27 +28,51 @@ fn main() {
         .expect("An error occurred while taking input");
 
     let secret_str = secret_str.trim().replace(" ", "").to_uppercase();
-    let secret = Secret::Encoded(secret_str)
-        .to_bytes()
-        .expect("Invalid Base32 secret");
+    let mut secrets: Vec<Vec<u8>> = Vec::new();
+    if secret_str.to_lowercase() != "n" {
+        secrets.push(
+            Secret::Encoded(secret_str)
+                .to_bytes()
+                .expect("Your secret was invalid."),
+        );
+    }
 
-    let totp = TOTP::new(Algorithm::SHA1, 6, 1, 30, secret).unwrap();
+    for var in env::vars() {
+        let secret = Secret::Encoded(var.1).to_bytes();
+        match secret {
+            Ok(secret) => secrets.push(secret),
+            _ => (),
+        }
+    }
+
+    let mut totps: Vec<TOTP> = Vec::new();
+
+    for secret in secrets {
+        let totp = TOTP::new(Algorithm::SHA1, 6, 1, 30, secret);
+        match totp {
+            Ok(totp) => totps.push(totp),
+            _ => (),
+        };
+    }
 
     let mut i: u8 = 30;
 
     loop {
-        let token = totp.generate_current().unwrap();
         clearscreen::clear().expect("failed to clear screen");
         println!("{}", title_screen_art);
-        println!(
-            "Current token: {} - {} seconds",
-            token,
-            30 - (SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs()
-                % 30)
-        );
+        println!("Missing a token? Check your environment variables to see if it was set properly");
+        for totp in &totps {
+            let token = totp.generate_current().unwrap();
+            println!(
+                "Current token: {} - {} seconds",
+                token,
+                30 - (SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs()
+                    % 30)
+            );
+        }
         thread::sleep(Duration::from_secs(1));
         if i > 0 {
             i -= 1;
